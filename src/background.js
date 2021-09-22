193abb80315b75a76dc30e4d2ffe524c8fe0fdc5
Galtz <galtz0321@gmail.com>
2021-09-22 23:18:54 +0800

    fix: eslint errors

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+const MENU_ID = 'MARKDOWN_COPIER_MENU_ID'
+
 const isNil = x => x == null
 
 const toMarkdownFormat = (title, url) => {
@@ -10,6 +12,7 @@ const sendCopyAction = (tabId, markdownLink) =>
   chrome.tabs.sendMessage(
     tabId,
     { action: 'COPY', payload: markdownLink },
+    // eslint-disable-next-line no-console
     response => console.log(`copy ${response.result}`)
   )
 
@@ -19,12 +22,21 @@ const handleBrowserAction = tab =>
 const handleMenuClick = (info, tab) =>
   sendCopyAction(tab.id, toMarkdownFormat(info.selectionText, info.linkUrl))
 
-chrome.browserAction.onClicked.addListener(handleBrowserAction)
+chrome.runtime.onInstalled.addListener(() => {
+  /* eslint-disable-next-line no-console */
+  chrome.contextMenus.create({
+    id: MENU_ID,
+    title: 'Copy Url as [title](url) format',
+    contexts: ['link'],
+    onclick: handleMenuClick
+  })
 
-chrome.contextMenus.create({
-  title: 'Copy Url as [title](url) format',
-  contexts: ['link'],
-  onclick: handleMenuClick
+  // eslint-disable-next-line no-console
+  console.log('markdown copier background script loaded.')
 })
 
-console.log('markdown copier background script loaded.')
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === MENU_ID) handleMenuClick(info, tab)
+})
+
+chrome.browserAction.onClicked.addListener(handleBrowserAction)
